7aad034fb507bc8dba83b35a25da453aa3cd8266
arpruss <arpruss@gmail.com>
2018-09-04 09:20:48 -0500

    foo

--- distr.bat
+++ distr.bat
@@ -1,2 +1,2 @@
-c:\python36\Scripts\pyinstaller --onefile --distpath win-64bit 3d.py
-c:\python37-32\Scripts\pyinstaller --onefile --distpath win-32bit 3d.py
+c:\python36\Scripts\pyinstaller --onefile --clean --distpath win-64bit 3d.py
+c:\python36-32\Scripts\pyinstaller --onefile --clean --distpath win-32bit 3d.py
